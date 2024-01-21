973fd89ee6a9b46657c56d8bf9f588e33907d29e
Belosheykindv <Belosheykindv@users.noreply.github.com>
2024-01-21 15:08:41 +0500

    createGitHub-PagesBuild

--- src/Components/Dialogs/Message/Message.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import s from './../Dialogs.module.css';
-
-const Message = (props) => {
-    return <div className={s.messages}>{props.message} <hr /></div>
-}
-export default Message;
