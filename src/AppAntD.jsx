65b480982d7c7f96772b567323011203a31e60d8
Belosheykindv <Belosheykindv@users.noreply.github.com>
2025-02-27 16:16:11 +0500

    added prettier config

--- src/AppAntD.jsx
+++ src/AppAntD.jsx
@@ -1,63 +1,68 @@
-import React from 'react';
-import './App.scss';
-import { Provider } from 'react-redux';
-import store from './Redux/reduxStore';
-import { BrowserRouter } from 'react-router-dom';
-import { HashRouter } from 'react-router-dom';
-import { Suspense } from 'react';
+import React from 'react'
+import './App.scss'
+import { Provider } from 'react-redux'
+import store from './Redux/reduxStore'
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from 'react-router-dom'
+import { Suspense } from 'react'
 // import Header from './Components/Header/header';
-import { NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom'
 import { logout } from './Redux/authReducer'
 // import ProfileContainer from './Components/Profile/profileContainer';
-import { Route, Routes } from 'react-router-dom';
-import BeerComp from './Components/Settings/Settings';
+import { Route, Routes } from 'react-router-dom'
+import BeerComp from './Components/Settings/Settings'
 // import DialogsContainer from './Components/Dialogs/Dialogs-container';
 // import UsersContainer from './Components/Users/usersContainer';
-import MusicContainer from './Components/Music/Music';
-import Login from './Components/Login/login';
+import MusicContainer from './Components/Music/Music'
+import Login from './Components/Login/login'
 import Login2 from './Components/Login/loginRFF'
 import ToDO from './Components/ToDO/todo'
-import { connect } from 'react-redux';
-import { compose } from 'redux';
-import { withRouter } from './Hoc/withRouter';
-import { initializedApp } from './Redux/appReducer';
-import Preloader from './Components/Common/Preloader/preloader';
-import { useEffect, useState } from 'react';
+import { connect } from 'react-redux'
+import { compose } from 'redux'
+import { withRouter } from './Hoc/withRouter'
+import { initializedApp } from './Redux/appReducer'
+import Preloader from './Components/Common/Preloader/preloader'
+import { useEffect, useState } from 'react'
 import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
-  UploadOutlined,
+  // UploadOutlined,
   UserOutlined,
-  VideoCameraOutlined,
-} from '@ant-design/icons';
-import { Layout, Menu, Button, theme } from 'antd';
-import { Footer } from 'antd/es/layout/layout';
-import HookForm from './Components/HookFrom/hookForm';
+  // VideoCameraOutlined,
+} from '@ant-design/icons'
+import { Layout, Menu, Button, theme } from 'antd'
+import { Footer } from 'antd/es/layout/layout'
+import HookForm from './Components/HookFrom/hookForm'
 
 // import { ChatPage } from './Pages/Chat/chatPage';
 
-const { Header, Sider, Content } = Layout;
-const ProfileContainer = React.lazy(() => import('./Components/Profile/profileContainer'));
-const DialogsContainer = React.lazy(() => import('./Components/Dialogs/Dialogs-container'));
-const UsersContainer = React.lazy(() => import('./Components/Users/usersContainer'));
+const { Header, Sider, Content } = Layout
+const ProfileContainer = React.lazy(
+  () => import('./Components/Profile/profileContainer'),
+)
+const DialogsContainer = React.lazy(
+  () => import('./Components/Dialogs/Dialogs-container'),
+)
+const UsersContainer = React.lazy(
+  () => import('./Components/Users/usersContainer'),
+)
 const ChatPage = React.lazy(() => import('./Pages/Chat/chatPage'))
 // const MusicContainer = React.lazy(() => import('./Components/Music/Music'))
 
 const App = (props) => {
-  const [collapsed, setCollapsed] = useState(false);
-  const { token: { colorBgContainer, borderRadiusLG } } = theme.useToken();
+  const [collapsed, setCollapsed] = useState(false)
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken()
   useEffect(() => {
-    props.initializedApp();
+    props.initializedApp()
   }, [])
   if (!props.store.app.initialize) {
     return <Preloader />
   } else
     return (
-      <Layout
-        className={`${collapsed ? 'app-wrapper-on' : 'app-wrapper-off'}`}
-      >
-        <Sider trigger={null} collapsible collapsed={collapsed}
-        >
+      <Layout className={`${collapsed ? 'app-wrapper-on' : 'app-wrapper-off'}`}>
+        <Sider trigger={null} collapsible collapsed={collapsed}>
           <Menu
             theme="dark"
             mode="inline"
@@ -66,54 +71,54 @@ const App = (props) => {
               {
                 key: '1',
                 icon: <UserOutlined />,
-                label: <NavLink to='/profile'>Профиль</NavLink>,
+                label: <NavLink to="/profile">Профиль</NavLink>,
               },
               {
                 key: '2',
                 icon: <UserOutlined />,
-                label: <NavLink to='/dialogs'>Сообщения</NavLink>,
+                label: <NavLink to="/dialogs">Сообщения</NavLink>,
               },
               {
                 key: '3',
                 icon: <UserOutlined />,
-                label: <NavLink to='/hookForm'>Hook Form + RTK</NavLink>,
+                label: <NavLink to="/hookForm">Hook Form + RTK</NavLink>,
               },
               {
                 key: '4',
                 icon: <UserOutlined />,
-                label: <NavLink to='/ToDO'>Список дел</NavLink>,
+                label: <NavLink to="/ToDO">Список дел</NavLink>,
               },
               {
                 key: '5',
                 icon: <UserOutlined />,
-                label: <NavLink to='/chat'>Чат (ws)</NavLink>,
+                label: <NavLink to="/chat">Чат (ws)</NavLink>,
               },
               {
                 key: '6',
                 icon: <UserOutlined />,
-                label: <NavLink to='/music'>Музыка</NavLink>,
+                label: <NavLink to="/music">Музыка</NavLink>,
               },
               {
                 key: '7',
                 icon: <UserOutlined />,
-                label: <NavLink to='/settings'>Пиво</NavLink>,
+                label: <NavLink to="/settings">Пиво</NavLink>,
               },
               {
                 key: '8',
                 icon: <UserOutlined />,
-                label: <NavLink to='/users'>Пользователи</NavLink>,
-              }
-            ]} />
+                label: <NavLink to="/users">Пользователи</NavLink>,
+              },
+            ]}
+          />
         </Sider>
         <Layout>
           <Header
-            id='header'
+            id="header"
             style={{
               padding: 0,
               background: colorBgContainer,
             }}
           >
-
             <Button
               type="text"
               icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
@@ -124,65 +129,69 @@ const App = (props) => {
                 height: 64,
               }}
             />
-            {props.isAuth
-              ? <span>{props.store.auth.login} <button onClick={props.logout}>Log out</button></span>
-              : <NavLink to='/login'>Логин</NavLink>}
-
-          </Header >
+            {props.isAuth ? (
+              <span>
+                {props.store.auth.login}{' '}
+                <button onClick={props.logout}>Log out</button>
+              </span>
+            ) : (
+              <NavLink to="/login">Логин</NavLink>
+            )}
+          </Header>
           <Content
-            id='content'
+            id="content"
             style={{
               margin: '24px 16px',
               padding: 24,
               minHeight: 280,
               background: colorBgContainer,
-              borderRadius: borderRadiusLG
+              borderRadius: borderRadiusLG,
             }}
           >
-
             <Routes>
-              <Route path='/dialogs' element={<DialogsContainer />} />
+              <Route path="/dialogs" element={<DialogsContainer />} />
               <Route path="/profile" element={<ProfileContainer />}>
-                <Route path=":userId"
-                  element={<ProfileContainer />} />
+                <Route path=":userId" element={<ProfileContainer />} />
               </Route>
-              <Route path='chat' element={<ChatPage />} />
-              <Route path='/music' element={<MusicContainer />} />
-              <Route path='/settings' element={<BeerComp />} />
-              <Route path='/users' element={<UsersContainer />} />
-              <Route path='/todo' element={<ToDO />} />
-              <Route path='/login' element={<Login2 />} />
-              <Route path='/login2' element={<Login />} />
-              <Route path='/hookForm' element={<HookForm />} />
-              <Route path='/' element={<Login2 />} />
+              <Route path="chat" element={<ChatPage />} />
+              <Route path="/music" element={<MusicContainer />} />
+              <Route path="/settings" element={<BeerComp />} />
+              <Route path="/users" element={<UsersContainer />} />
+              <Route path="/todo" element={<ToDO />} />
+              <Route path="/login" element={<Login2 />} />
+              <Route path="/login2" element={<Login />} />
+              <Route path="/hookForm" element={<HookForm />} />
+              <Route path="/" element={<Login2 />} />
             </Routes>
           </Content>
           <Footer style={{ textAlign: 'center', fontSize: 30 }}>
             Частная компания - OOO "Сошл - нетворк"
           </Footer>
         </Layout>
-
-      </Layout >
-    );
-
+      </Layout>
+    )
 }
 const mapStateToProps = (state) => ({
   initialize: state.app.initialize,
   ownerId: state.auth.id,
-  isAuth: state.auth.isAuth
+  isAuth: state.auth.isAuth,
 })
 let AppContainer = compose(
   withRouter,
-  connect(mapStateToProps, { initializedApp, logout }))(App);
+  connect(mapStateToProps, { initializedApp, logout }),
+)(App)
 const SamuraiJSAPP = (props) => {
-  return <Provider store={store}>
-    <HashRouter>
-      <Suspense fallback={<Preloader />}>
-        <AppContainer
-          store={store.getState()}
-          dispatch={store.dispatch.bind(store)} />
-      </Suspense>
-    </HashRouter>
-  </Provider>
+  return (
+    <Provider store={store}>
+      <HashRouter>
+        <Suspense fallback={<Preloader />}>
+          <AppContainer
+            store={store.getState()}
+            dispatch={store.dispatch.bind(store)}
+          />
+        </Suspense>
+      </HashRouter>
+    </Provider>
+  )
 }
 export default SamuraiJSAPP
--- src/Pages/Chat/chatPage.jsx
+++ src/Pages/Chat/chatPage.jsx
@@ -1,104 +1,153 @@
-import React, { useEffect, useRef, memo } from "react"
-import { useState } from "react"
-import { NavLink } from "react-router-dom"
-import { Button, Input, theme } from "antd"
-import { useDispatch, useSelector } from "react-redux"
-import { sendMessage, startMessagesListening, startStatusListening, stopMessagesListening, stopStatusListening, unmountChat } from "../../Redux/chatReducer"
+import React, { useEffect, useRef } from 'react'
+import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import { Button, Input, theme } from 'antd'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  sendMessage,
+  startMessagesListening,
+  startStatusListening,
+  stopMessagesListening,
+  stopStatusListening,
+  unmountChat,
+} from '../../Redux/chatReducer'
 import style from './/..//../Components/Dialogs/Dialogs.module.css'
-import { withAuthRedirect } from "../../Hoc/withAuthRedirect"
-import { compose } from "redux"
+import { withAuthRedirect } from '../../Hoc/withAuthRedirect'
+import { compose } from 'redux'
 import userPhoto from './/..//..//Images/userPhoto.png'
 const ChatPage = () => {
-    return <div>
-        <Chat />
+  return (
+    <div>
+      <Chat />
     </div>
+  )
 }
 
 const Chat = () => {
-    const dispatch = useDispatch()
-    useEffect(() => {
-        dispatch(startMessagesListening())
-        dispatch(startStatusListening())
-        return () => {
-            dispatch(stopMessagesListening())
-            dispatch(stopStatusListening())
-            dispatch(unmountChat())
-        }
-    }, [])
-    return <div>
-        <Messages />
-        <AddMessageForm />
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(startMessagesListening())
+    dispatch(startStatusListening())
+    return () => {
+      dispatch(stopMessagesListening())
+      dispatch(stopStatusListening())
+      dispatch(unmountChat())
+    }
+  }, [])
+  return (
+    <div>
+      <Messages />
+      <AddMessageForm />
     </div>
+  )
 }
 const Messages = () => {
-    const messages = useSelector((state => state.chat.messages))
-    return <div style={{ height: '800px', overflowY: 'auto' }}>
-        {messages.map((m, index) => <Message key={m.id} message={m} id={m} />)}
+  const messages = useSelector((state) => state.chat.messages)
+  return (
+    <div style={{ height: '800px', overflowY: 'auto' }}>
+      {messages.map((m, index) => (
+        <Message key={m.id} message={m} id={m} />
+      ))}
     </div>
+  )
 }
 const Message = React.memo(({ message }) => {
-    console.log('Отрисовка Сообщений')
-    const { token: { colorBgContainer, borderRadiusLG } } = theme.useToken();
-    const messagesEndRef = useRef(null)
-    const scrollToBottom = () => {
-        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
-    }
-    useEffect(() => {
-        scrollToBottom()
-    }, [message]);
+  console.log('Отрисовка Сообщений')
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken()
+  const messagesEndRef = useRef(null)
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' })
+  }
+  useEffect(() => {
+    scrollToBottom()
+  }, [message])
 
-    return <div style={{
+  return (
+    <div
+      style={{
         margin: '4px 6px',
         padding: 4,
         // minHeight: 280,
         background: colorBgContainer,
-        borderRadius: borderRadiusLG
-    }} ref={messagesEndRef}>
-        <NavLink to={'/profile/' + message.userId}><img style={{ height: 50, width: 40, borderRadius: '10px' }} src={message.photo || userPhoto} /></NavLink>
-        <span style={{ position: 'relative', bottom: '20px', left: '10px', fontSize: '1.2rem' }}>{message.userName} </span>
-        <br />
-        <span style={{ fontSize: '1.0rem' }} >{message.message}</span>
-        {/* <br /> */}
-        <hr />
+        borderRadius: borderRadiusLG,
+      }}
+      ref={messagesEndRef}
+    >
+      <NavLink to={'/profile/' + message.userId}>
+        <img
+          style={{ height: 50, width: 40, borderRadius: '10px' }}
+          src={message.photo || userPhoto}
+        />
+      </NavLink>
+      <span
+        style={{
+          position: 'relative',
+          bottom: '20px',
+          left: '10px',
+          fontSize: '1.2rem',
+        }}
+      >
+        {message.userName}{' '}
+      </span>
+      <br />
+      <span style={{ fontSize: '1.0rem' }}>{message.message}</span>
+      {/* <br /> */}
+      <hr />
     </div>
+  )
 })
 const AddMessageForm = () => {
-    const [message, setMessage] = useState()
-    const dispatch = useDispatch()
-    const status = useSelector((state) => state.chat.status)
-    const inputRef = useRef(null)
-    const [btnStatus, setButtonStatus] = useState(false)
-    const sendMessageHandler = () => {
-        if (!message) {
-            return setButtonStatus(false)
-        }
-        dispatch(sendMessage(message))
-        setMessage('')
-        setButtonStatus(false)
+  const [message, setMessage] = useState()
+  const dispatch = useDispatch()
+  const status = useSelector((state) => state.chat.status)
+  const inputRef = useRef(null)
+  const [btnStatus, setButtonStatus] = useState(false)
+  const sendMessageHandler = () => {
+    if (!message) {
+      return setButtonStatus(false)
     }
-    const inputHandler = (e) => {
-        if (e == '') { setButtonStatus(false) }
-        else
-            setButtonStatus(true)
-    }
-    return <div>
-        <div style={{ display: 'flex', marginLeft:'10px' }}>
-            <Input size="large" showCount maxLength={100} onInput={(e) => setMessage(e.currentTarget.value)} ref={inputRef} name={'chatInput'} onChange={(e) => inputHandler(e.currentTarget.value)} value={message} placeholder="Введите сообщение" ></Input>
-            <Button
-                className={btnStatus ? style.btn : style.disBtn}
-                disabled={status !== 'ready' && btnStatus !== true}
-                onClick={sendMessageHandler}>Отправить</Button>
-        </div>
-        <br></br>
-        {/* <div>
+    dispatch(sendMessage(message))
+    setMessage('')
+    setButtonStatus(false)
+  }
+  const inputHandler = (e) => {
+    if (e == '') {
+      setButtonStatus(false)
+    } else setButtonStatus(true)
+  }
+  return (
+    <div>
+      <div style={{ display: 'flex', marginLeft: '10px' }}>
+        <Input
+          size="large"
+          showCount
+          maxLength={100}
+          onInput={(e) => setMessage(e.currentTarget.value)}
+          ref={inputRef}
+          name={'chatInput'}
+          onChange={(e) => inputHandler(e.currentTarget.value)}
+          value={message}
+          placeholder="Введите сообщение"
+        ></Input>
+        <Button
+          className={btnStatus ? style.btn : style.disBtn}
+          disabled={status !== 'ready' && btnStatus !== true}
+          onClick={sendMessageHandler}
+        >
+          Отправить
+        </Button>
+      </div>
+      <br></br>
+      {/* <div>
             <button
                 className={btnStatus ? style.btn : style.disBtn}
                 disabled={status !== 'ready' && btnStatus !== true}
                 onClick={sendMessageHandler}>Отправить</button>
         </div> */}
     </div>
+  )
 }
 // export default ChatPage;
-export default compose(
-    withAuthRedirect
-)(ChatPage)
+export default compose(withAuthRedirect)(ChatPage)
